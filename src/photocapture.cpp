f16b666e4fbcc2bd1036863abfb4e5fb688c1355
David McCann <mccann.dmc@gmail.com>
2018-01-13 18:09:17 +0100

    added mock-capture mode

--- src/photocapture.cpp
+++ src/photocapture.cpp
@@ -3,13 +3,31 @@
 #include "settings.h"
 
 #include <QDebug>
+#include <QFile>
 #include <QProcess>
+#include <QThread>
+
+#define MOCK_CAPTURE
 
 Photo PhotoCapture::capture() {
   const QDateTime now = QDateTime::currentDateTime();
   const QString timestamp = now.toString("yyyyMMdd-hhmmsszzz");
   const QString filename = QString("%2.jpeg").arg(timestamp);
+#ifdef MOCK_CAPTURE
+  static int count = 0;
+  ++count;
+  QString mockFile = Settings::instance().tempPath() + "/mock" +
+                     QString::number((count % 4) + 1) + ".jpeg";
+  QString newFile = Settings::instance().tempPath() + "/" + filename;
+  QFile::copy(mockFile, newFile);
 
+  QThread::sleep(1);
+
+  Photo photo;
+  photo.setPath(newFile);
+
+  return photo;
+#else
   QProcess process;
   process.setWorkingDirectory(Settings::instance().tempPath());
   process.setProgram("/usr/local/bin/gphoto2");
@@ -24,4 +42,5 @@ Photo PhotoCapture::capture() {
   photo.setPath(Settings::instance().tempPath() + "/" + filename);
 
   return photo;
+#endif
 }
